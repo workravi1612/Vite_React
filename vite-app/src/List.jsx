4f56028281e9a91860ec204a12ed31972c1f6b88
ravi <work.ravivyas@gmail.com>
2024-10-11 00:36:55 +0530

    Filter Object

--- vite-app/src/List.jsx
+++ vite-app/src/List.jsx
@@ -7,9 +7,32 @@ const fruits = [{id : 1, name : "apple", calories : 95},
                 {id : 4, name : "coconut", calories: 159},
                 {id : 5, name : "pineapple", calories : 37}];
 
-    const listItems = fruits.map(fruit => <li key = {fruit.id}>{fruit.name} : &nbsp;
-                                                    <b> {fruit.calories}</b></li>);
+    // fruits.sort((a,b) => a.name.localeCompare(b.name)); //Alphabetical sorting
+    // fruits.sort((a,b) => b.name.localeCompare(a.name)); //Reverse Alphabetical sorting
+    // fruits.sort((a,b) => a.calories - b.calories) //Numerical Order
+    // fruits.sort((a,b) => b.calories - a.calories) //Reverse Numerical Order
 
-    return(<ol>{listItems}</ol>)
+    
+    
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100) //To fillter fruits which has low calories than 100
+    
+    
+    // const listItems = lowCalFruits.map(lowCalFruit => <li key = {lowCalFruit.id}>
+    //                                                   {lowCalFruit.name} : &nbsp;
+    //                                                   <b> {lowCalFruit.calories}</b></li>);
+    
+    // const highCalFruits = fruits.filter(fruit => fruit.calories >= 100) //To fillter fruits which has high calories than 100
+    
+    // const listItems = highCalFruits.map(highCalFruit => <li key = {highCalFruit.id}>
+    //                                                   {highCalFruit.name} : &nbsp;
+    //                                                   <b> {highCalFruit.calories}</b></li>);
+    
+    
+    const listItems = fruits.map(fruit => <li key = {fruit.id}>
+                                        {fruit.name} : &nbsp;
+                                        <b> {fruit.calories}</b></li>);
+                                        
+
+return(<ol>{listItems}</ol>);
 }
 export default List
