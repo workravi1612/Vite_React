e953d483383887e3e4a9041b67086920de48ecbf
ravi <work.ravivyas@gmail.com>
2024-10-11 14:25:03 +0530

    onClick with DOM

--- vite-app/src/Button.jsx
+++ vite-app/src/Button.jsx
@@ -10,11 +10,26 @@ function Button(){
             cursor: "pointer",
     }
 
-    const handleClick = () => console.log("OUCH !")
-    const handleClick2 = (name) => console.log(`${name} stop clicking me`)
+    // const handleClick = () => console.log("OUCH !")
+    // const handleClick2 = (name) => console.log(`${name} stop clicking me`)
+    // let count = 0;
+    // const handleClick = (name) => {
+    //     if(count < 3){
+    //         count++;
+    //         console.log(`${name} clicked me ${count} time/s`);
+    //     }else{
+    //         console.log(`${name} stop clicking me!`);
+    //     }
+    // }
 
+    const handleClick = (e) => {
+        // console.log(e)
+        e.target.textContent = "Hello";
+    }
     return(
-        <button style={styles} onClick={ () => handleClick2("Hey")}>Click me</button>
+        // <button style={styles} onClick={ () => handleClick("Hey")}>Click me</button>
+        // Also can use doubleClick rather than onClick
+        <button style={styles} onClick={ (e) => handleClick(e)}>Click me</button>
         // <button onClick={handleClick}>Click me</button>
     )
 
