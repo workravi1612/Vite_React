72ba447572e407f20b01a67ab042165ccbea63a0
ravi <work.ravivyas@gmail.com>
2024-10-11 11:42:12 +0530

    Reusable list component

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -9,7 +9,14 @@ import List from'./List.jsx'
 
 
 function App() {
+  
+const fruits = [{id : 1, name : "apple", calories : 95}, 
+                {id : 2, name : "orange", calories : 45},
+                {id : 3, name : "banana", calories : 105},
+                {id : 4, name : "coconut", calories: 159},
+                {id : 5, name : "pineapple", calories : 37}];
   return(
+    <>
     <>
     {/* <Header/>
     <Food/>
@@ -25,7 +32,8 @@ function App() {
     <Student name="Sandy" age={27} isStudent={true}/>
     <Student />
     <UserGreeting isLoggedIn={true} username="Ravi"/> */}
-    <List />
+    </>
+    <List items={fruits} category= "Fruits" />
     </>
   );
 }
--- vite-app/src/List.jsx
+++ vite-app/src/List.jsx
@@ -1,11 +1,11 @@
 
-function List(){
+function List(props){
 
-const fruits = [{id : 1, name : "apple", calories : 95}, 
-                {id : 2, name : "orange", calories : 45},
-                {id : 3, name : "banana", calories : 105},
-                {id : 4, name : "coconut", calories: 159},
-                {id : 5, name : "pineapple", calories : 37}];
+// const fruits = [{id : 1, name : "apple", calories : 95}, 
+//                 {id : 2, name : "orange", calories : 45},
+//                 {id : 3, name : "banana", calories : 105},
+//                 {id : 4, name : "coconut", calories: 159},
+//                 {id : 5, name : "pineapple", calories : 37}];
 
     // fruits.sort((a,b) => a.name.localeCompare(b.name)); //Alphabetical sorting
     // fruits.sort((a,b) => b.name.localeCompare(a.name)); //Reverse Alphabetical sorting
@@ -27,12 +27,22 @@ const fruits = [{id : 1, name : "apple", calories : 95},
     //                                                   {highCalFruit.name} : &nbsp;
     //                                                   <b> {highCalFruit.calories}</b></li>);
     
+    const category = props.category;
+    const itemList = props.items;
     
-    const listItems = fruits.map(fruit => <li key = {fruit.id}>
-                                        {fruit.name} : &nbsp;
-                                        <b> {fruit.calories}</b></li>);
+
+    const listItems = itemList.map(item => <li key = {item.id}>
+                                            {item.name} : &nbsp;
+                                            <b> {item.calories}</b></li>); 
+
+    // const listItems = fruits.map(fruit => <li key = {fruit.id}>
+    //                                     {fruit.name} : &nbsp;
+    //                                     <b> {fruit.calories}</b></li>);
                                         
 
-return(<ol>{listItems}</ol>);
+return(<>
+        <h3>{category}</h3>
+        <ol>{listItems}</ol>
+        </>);
 }
 export default List
