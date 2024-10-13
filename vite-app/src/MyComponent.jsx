4193b568c5a2c3a291a739c83b185b0bc074f9cb
ravi <work.ravivyas@gmail.com>
2024-10-13 17:18:11 +0530

    useState

--- vite-app/src/MyComponent.jsx
+++ vite-app/src/MyComponent.jsx
@@ -1,16 +1,32 @@
 import React, {useState} from 'react';
 function MyComponent(){
 
-    const [name, setName] = useState();
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
 
     const updateName = () => {
         setName("Spongebob")
+    }
+
+    const incrementAge = () => {
+        setAge(age+1)
+    }
+
+    const toggleEmployeeStatus = () => {
+        setIsEmployed(!isEmployed)
     }
 
     return(
         <div>
             <p>Name : {name}</p>
             <button onClick={updateName}>Set Name</button>
+
+            <p>Age : {age}</p>
+            <button onClick={incrementAge}>Increment Age</button>
+
+            <p>Is employed : {isEmployed ? "Yes" : "No"}</p>
+            <button onClick={toggleEmployeeStatus}>Toggle Status</button>
         </div>
     )
 }
